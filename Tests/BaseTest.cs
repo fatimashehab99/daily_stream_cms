2770f4606b89207826c67837ecbe9581bcdfdbc6
fatimashehab99 <fatima_shehab_sh@outlook.com>
2025-04-28 01:29:39 +0300

    roles unit test

---
+++ Tests/BaseTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using daily_stream_cms.Services;
+using daily_stream_cms.Models;
+
+
+namespace daily_stream_cms.Tests
+{
+    [TestFixture]
+    public class BaseTest
+    {
+        protected AppDbContext _context;
+        protected RolesService _rolesServices;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new AppDbContext(options);
+
+            _rolesServices = new RolesService(_context);
+        }
+
+        protected void SeedRoles()
+        {
+            var roles = new List<Role>
+            {
+                new Role { Name = "Admin" },
+                new Role { Name = "Editor" },
+                new Role { Name = "Viewer" }
+            };
+
+            _context.Roles.AddRange(roles);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Tests/RolesTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using daily_stream_cms.Services;
+using daily_stream_cms.Models;
+
+namespace daily_stream_cms.Tests
+{
+    public class RolesTest :BaseTest
+    {
+        [Test]
+        public void getAllRolesShouldReturnAllRoles()
+        {
+            // Arrange
+            SeedRoles();
+
+            // Act
+            var roles = _rolesServices.getAllRoles();
+
+            // Assert
+            roles.Should().NotBeNull();
+            roles.Should().HaveCount(3);
+        } 
+
+    }
+}
